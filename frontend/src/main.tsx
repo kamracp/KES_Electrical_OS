7a23294d889331972b38fe3b496f08df338b28d6
Chander Prakash Kamra <chander@gmail.com>
2026-09-03 20:35:40 +0000

    KESE-FE-S1-M11: Activate frontend application router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import { App } from "./App";
+import { router } from "./app/router";
 
 const rootElement = document.getElementById("root");
 
@@ -11,6 +12,6 @@ if (rootElement === null) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
