9987dd810b442aa12ed807054f71abbc2bb872ff
Chander Prakash Kamra <chander@gmail.com>
2026-09-04 09:08:46 +0000

    KESE-FE-S2-M26: Register fault study route

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { App } from "../App";
+import { FaultStudyPage } from "../pages/FaultStudyPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/fault-study",
+    element: <FaultStudyPage />,
   },
 ]);
